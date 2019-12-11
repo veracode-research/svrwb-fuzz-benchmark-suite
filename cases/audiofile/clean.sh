4db59cfd6cce5f93a85071d88ca58eac5fcf213e
Andrew R Reiter <arr@pop-os.localdomain>
2019-12-11 13:19:00 -0500

    Remove autom4te.cache and update the clean script to perform this each clean

--- cases/audiofile/clean.sh
+++ cases/audiofile/clean.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+SRC=audiofile
+
 #rm -f lame_crash_UNK010.mp3 lame_crash_UNK011.mp3 lame_crash_UNK012.mp3 *.mp3.mp3
-pushd audiofile
+pushd ${SRC}
 make clean
 make distclean
 rm -rf install
+rm -rf audiofile/autom4te.cache/
 popd
 
