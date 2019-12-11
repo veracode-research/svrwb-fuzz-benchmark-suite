d01a499b8f3c43495d891e09f35e080c2a21f286
Andrew R Reiter <arr@pop-os.localdomain>
2019-12-11 13:34:44 -0500

    Clean up some build remnants of libarchive and don't use symlink (bc git)

--- cases/libarchive/clean.sh
+++ cases/libarchive/clean.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-pushd libarchive
+pushd libarchive+81ce2c24f9fec640740de9bcea920ab71ef89059
 make clean
 make distclean
 rm -rf install
+rm -rf Makefile.in bsdcat build   \
+	cat/test/list.h  \
+        config.h.in  \
+        config.log  \
+        configure   \
+        autom4te.cache/
 popd
