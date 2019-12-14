ab6147169638229af5605c88d64511d7cfc131bc
Andrew R. Reiter <arr@watson.org>
2019-12-13 20:40:45 -0500

    Add optional GCC use. GCC build breaks on audiofile, but works on other. So, this works, but must fix that build.

--- cases/config.sh
+++ cases/config.sh
@@ -11,20 +11,34 @@
 # TODO: Should include gcc as well.
 
 LLVM_HOME=/usr
+GCC_HOME=/usr
 
 AFL_HOME=/home/arr/AFLplusplus-git
 
 #
 # Set to non-zero for AFL compilation mode
 #
-AFL_ON=1
+AFL_ON=0
+
+#
+# Turn to non-zero to enable GCC
+#
+USE_GCC=0
+
 
 #
 # These will be the value passed to AFL_CC and AFL_CXX
 # when building with CC as afl-${CC}
 #
-ACC=${LLVM_HOME}/bin/clang
-ACXX=${LLVM_HOME}/bin/clang++
+if [ "$USE_GCC" -eq "0" ]; then
+ echo "Using CLANG"
+ ACC=${LLVM_HOME}/bin/clang
+ ACXX=${LLVM_HOME}/bin/clang++
+else
+ echo "Using GCC"
+ ACC=${GCC_HOME}/bin/gcc
+ ACXX=${GCC_HOME}/bin/g++
+fi
 
 #
 # Set LCC/LCXX for AFL vs no AFL as they will be CC and CXX
@@ -32,38 +46,48 @@ ACXX=${LLVM_HOME}/bin/clang++
 #
 if [ "$AFL_ON" -eq "0" ]; then
  echo "AFL compilation: disabled"
+ LCC=${ACC}
+ LCXX=${ACXX}
 
- LCC=${LLVM_HOME}/bin/clang
- LCXX=${LLVM_HOME}/bin/clang++
 else
  echo "AFL compilation: enabled"
+ if [ "$USE_GCC" -eq "0" ]; then
+  LCC=${AFL_HOME}/afl-clang-fast
+  LCXX=${AFL_HOME}/afl-clang-fast++
+  #LCC=${AFL_HOME}/afl-clang
+  #LCXX=${AFL_HOME}/afl-clang
 
- LCC=${AFL_HOME}/afl-clang-fast
- LCXX=${AFL_HOME}/afl-clang-fast++
-# LCC=${AFL_HOME}/afl-clang
-# LCXX=${AFL_HOME}/afl-clang
+ else
+  LCC=${AFL_HOME}/afl-gcc
+  LCXX=${AFL_HOME}/afl-g++
+ fi
 fi
 
 #
 # If you do not want ASan, make this empty
 #
 ASAN_BUILD="-fsanitize=address"
+#ASAN_BUILD=""
 
 #
 # If you want to compile for x86-64, just leave empty
 #
 IA32_TARGET="-m32 -march=i686"
+#IA32_TARGET=""
 
 #
 # Set these, hopefully, for CFLAGS and CXXFLAGS
 #
 # Note: 32-bit for ASan + AFL
 #
-LCFLAGS="${IA32_TARGET} -nopie -fno-stack-protector ${ASAN_BUILD}"
-LCXXFLAGS="${IA32_TARGET} -nopie -fno-stack-protector ${ASAN_BUILD}"
+if [ "$USE_GCC" -eq "0" ]; then
+ LCFLAGS="${IA32_TARGET} -nopie -fno-stack-protector ${ASAN_BUILD}"
+ LCXXFLAGS="${IA32_TARGET} -nopie -fno-stack-protector ${ASAN_BUILD}"
+else
+ LCFLAGS="${IA32_TARGET} -no-pie -fno-stack-protector ${ASAN_BUILD}"
+ LCXXFLAGS="${IA32_TARGET} -no-pie -fno-stack-protector ${ASAN_BUILD}"
+fi
 
-#LCFLAGS="-nopie -fno-stack-protector"
-#LCXXFLAGS="-nopie -fno-stack-protector"
 
 #
 # Set for any ./configure script options --prefix is done
